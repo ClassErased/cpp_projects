cadfae3002c92f5be8d5e9e70d4a7c8a7c54b9e0
ClassErased <aaron@aacyber.uk>
2022-07-17 18:06:43 +0100

    leap year calculator

---
+++ leapyear.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main() {
+  
+  int year = 0;
+  //ask user to enter year, must be 4 digits
+  std::cout << "Enter year: ";
+  std::cin >> year;
+  
+   if (year < 1000 || year > 9999) {
+ 
+    std::cout << "Invalid entry.\n";
+ 
+  }
+  //some logic n stuff
+  else if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+ 
+    std::cout << year;
+    std::cout << " falls on a leap year.\n";
+ 
+  }
+  else {
+ 
+    std::cout << year;
+    std::cout << " is not a leap year.\n";
+ 
+  }
+  
+}
